740248ca37a0b9fe0f4583233598c6baaa3b046a
VConstans <victor.constans@etu.unistra.fr>
2017-04-22 22:26:03 +0200

    Copy, paste and adapte client socket

--- client/guiserver.cpp
+++ client/guiserver.cpp
@@ -1,6 +1,93 @@
 #include "guiserver.h"
 
 GuiServer::GuiServer(QObject *parent) :
-    QObject(parent)
+     QLocalSocket(parent)
 {
 }
+
+QString GuiServer::quoted(const QString &s){
+    return "\""+s+"\"";
+}
+
+
+void GuiServer::observe_property(int id, const QString &prop){
+    const QString cmd[] = {"\"observe_property\"",
+                           QString::number(id),
+                           quoted(prop), NULL};
+    this->send_command(cmd);
+}
+
+void GuiServer::set_property(const QString &prop, int value){
+    const QString cmd[] = {"\"set_property\"", quoted(prop),
+                           QString::number(value), NULL};
+    this->send_command(cmd);
+}
+
+void GuiServer::set_property(const QString &prop, const QString &value){
+    const QString cmd[] = {"\"set_property\"", quoted(prop),
+                           quoted(value), NULL};
+    this->send_command(cmd);
+}
+
+void GuiServer::set_property(const QString &prop, bool value){
+    QString _value = value ? "true" : "false";
+    const QString cmd[] = {"\"set_property\"", quoted(prop),
+                           quoted(_value), NULL};
+    this->send_command(cmd);
+}
+
+void GuiServer::step_backward(){
+    const QString cmd[] = {"\"seek\"","-0.2","\"relative\"", NULL};
+    this->send_command(cmd);
+}
+
+void GuiServer::step_forward(){
+    const QString cmd[] = {"\"seek\"","0.2","\"relative\"", NULL};
+    this->send_command(cmd);
+}
+
+void GuiServer::pause(bool value){
+    this->set_property("pause",value);
+}
+
+void GuiServer::volume(int value){
+    this->set_property("volume",value);
+}
+
+void GuiServer::stop(){
+    const QString cmd[] = {"\"stop\"",NULL};
+    this->send_command(cmd);
+}
+
+void GuiServer::pl_next(){
+    const QString cmd[] = {"\"playlist-next\"",NULL};
+    this->send_command(cmd);
+}
+
+void GuiServer::pl_prev(){
+    const QString cmd[] = {"\"playlist-prev\"",NULL};
+    this->send_command(cmd);
+}
+
+void GuiServer::load_file(const QString &filename){
+    const QString cmd[] = {"\"loadfile\"",quoted(filename), NULL};
+    this->send_command(cmd);
+}
+
+
+void GuiServer::send_command(const QString cmd[]){
+
+    QString* json_cmd = new QString();
+    int i = 0;
+    while (cmd[i] != NULL){
+       json_cmd->append(cmd[i++]);
+       if (cmd[i] != NULL) json_cmd->append(",");
+    }
+
+    json_cmd->prepend("{\"command\":[");
+    json_cmd->append("]}\n");
+    qDebug() << *json_cmd;
+    this->write(json_cmd->toUtf8());
+}
+
+
--- client/guiserver.h
+++ client/guiserver.h
@@ -1,17 +1,31 @@
 #ifndef GUISERVER_H
 #define GUISERVER_H
 
-#include <QObject>
+#include <QLocalSocket>
 
-class GuiServer : public QObject
+class GuiServer : public QLocalSocket
 {
     Q_OBJECT
 public:
     explicit GuiServer(QObject *parent = 0);
+    QString quoted(const QString &s);
 
 signals:
 
 public slots:
+    void send_command(const QString cmd[]);
+    void observe_property(int id, const QString &prop);
+    void set_property(const QString &prop, const QString &value);
+    void set_property(const QString &prop, int value);
+    void set_property(const QString &prop, bool value);
+    void step_forward();
+    void step_backward();
+    void pause(bool value);
+    void volume(int value);
+    void stop();
+    void pl_next();
+    void pl_prev();
+    void load_file(const QString &filename);
 
 };
 
